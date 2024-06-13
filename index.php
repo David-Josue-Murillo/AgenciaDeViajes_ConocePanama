a255e45a5f9d1576fbaad76f63eb22dba61e2c69
David Murillo <david.josue.murillo23@gmail.com>
2024-06-13 12:23:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conoce Panamá</title>
+</head>
+<body>
+    <h1>Conoce Panamá</h1>
+    <p>Este es un sitio web que te permitirá conocer Panamá.</p>
+    <p>Para comenzar, selecciona una de las siguientes opciones:</p>
+    <ul>
+        <li><a href="provincias.php">Provincias</a></li>
+        <li><a href="distritos.php">Distritos</a></li>
+        <li><a href="corregimientos.php">Corregimientos</a></li>
+    </ul>
+</body>
+</html>
