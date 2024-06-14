9897775f2b47fef607be41264c681e58e6a13f4c
David Murillo <david.josue.murillo23@gmail.com>
2024-06-13 22:14:57 -0500

    actualizando imagenes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,13 @@
     <link rel="stylesheet" href="./assets/css/style.css">
     <title>Conoce Panamá</title>
 </head>
+
 <body>
     <div class="">
         <header>
             <div>
                 <div>
-                    <img src="./assets/img/logo.png" alt="Logo">
+                    <img src="./assets/img/logo.png" alt="Administrador">
                 </div>
                 <div>
                     <h2>Conoce Panamá</h2>
@@ -28,25 +30,40 @@
                         <a href="#">Paquetes</a>
                     </li>
                     <li>
-                        <a href="#">Cruceros</a>
+                        <a href="#">Transporte</a>
                     </li>
                     <li>
-                        <a href="#">Planes</a>
+                        <a href="#">Lugares</a>
                     </li>
                     <li>
                         <a href="#">Contacto</a>
                     </li>
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
                 </ul>
             </nav>
-            
+
             <div>
                 <div>
                     <button>
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-cog" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h2.5" />
+                                <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                <path d="M19.001 15.5v1.5" />
+                                <path d="M19.001 21v1.5" />
+                                <path d="M22.032 17.25l-1.299 .75" />
+                                <path d="M17.27 20l-1.3 .75" />
+                                <path d="M15.97 17.25l1.3 .75" />
+                                <path d="M20.733 20l1.3 .75" />
+                            </svg>
+                        </a>
+                    </button>
+
+                    <button>
                         <a href="#">Iniciar sesión</a>
                     </button>
+
                     <button>
                         <a href="#">Registrarse</a>
                     </button>
@@ -59,31 +76,42 @@
                 <div>
                     <p>Agencia de viajes en Panamá</p>
                     <h1>¿Listo para conocer a Panamá?</h1>
-                    <p>Que esperas, reserva un paseo YAA!</p>
+                    <p>Que esperas, reserva tu viaje YAA!</p>
                 </div>
 
                 <section>
                     <button>
-                        <a href="#">Cotiza y Reserva</a>
+                        <a href="#">Cotizar</a>
                     </button>
-                    
+
                     <button>
-                        <a href="#">Cruceros</a>
+                        <a href="#">Reservar</a>
                     </button>
                 </section>
             </section>
 
             <section class="section__paquetes">
                 <div>
-                    <h2>Conoce nuestros paquetes vacacionales</h2>
+                    <h2>Conoce nuestros paquetes de viajes</h2>
                     <p>Descubre las mejores opciones de viajes que ofrecemos en Panamá</p>
                 </div>
 
                 <section class="paquetes__img">
-
+                    <div>
+                        <div>
+                            <img src="./assets/img/paquete_1.webp" alt="Vuelo a Bocas del Toro">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_2.webp" alt="Ciudad de Panamá">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_3.jpeg" alt="Casco Antiguo">
+                        </div>
+                    </div>
                 </section>
             </section>
         </main>
     </div>
 </body>
+
 </html>
