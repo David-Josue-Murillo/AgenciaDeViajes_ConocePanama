144096aa3f8305de2ef9912de605140d1b4640ea
David Murillo <david.josue.murillo23@gmail.com>
2024-06-14 19:19:30 -0500

    maquetación de pagiba destinos

---
+++ destinos.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Conoce Panamá - Paquetes</title>
+</head>
+
+<body>
+    <div class="">
+        <header>
+            <div>
+                <div>
+                    <img src="./assets/img/logo.png" alt="Administrador">
+                </div>
+                <div>
+                    <h2>Conoce Panamá</h2>
+                </div>
+            </div>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="./index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="./paquetes.php">Paquetes</a>
+                    </li>
+                    <li>
+                        <a href="./transporte.php">Cruceros</a>
+                    </li>
+                    <li>
+                        <a href="./destinos.php">Destinos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div>
+                <div>
+                    <button>
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-cog" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h2.5" />
+                                <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                <path d="M19.001 15.5v1.5" />
+                                <path d="M19.001 21v1.5" />
+                                <path d="M22.032 17.25l-1.299 .75" />
+                                <path d="M17.27 20l-1.3 .75" />
+                                <path d="M15.97 17.25l1.3 .75" />
+                                <path d="M20.733 20l1.3 .75" />
+                            </svg>
+                        </a>
+                    </button>
+
+                    <button>
+                        <a href="#">Iniciar sesión</a>
+                    </button>
+
+                    <button>
+                        <a href="#">Registrarse</a>
+                    </button>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <section class="section__content__destinos">
+                <div>
+                    <h1>Explorando Panamá a través de nuestros atractivos destinos turisticos</h1>
+                    <p>Beneficiate de nuestras ofertas especiales para descubrir los mejores sitios atractivos de Panamá</p>
+                </div>
+
+                <div>
+                    <button>
+                        <a href="#">Lugares turísticos</a>
+                    </button>
+
+                    <button>
+                        <a href="#">Acerca de Panamá</a>
+                    </button>
+                </div>
+            </section>
+
+            <section class="section__paquetes">
+                <!-- Todos los paquetes de viajes  -->
+                <section class="paquetes__img">
+                    <div>
+                        <div>
+                            <img src="./assets/img/paquete_1.webp" alt="Bocas del Toro">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_2.webp" alt="Isla Contadora">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_3.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_4.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_5.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_6.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_7.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_8.webp" alt="Isla Taboga">
+                        </div>
+                        <div>
+                            <img src="./assets/img/paquete_9.webp" alt="Isla Taboga">
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div>
+                        <h2>Blog de Panamá</h2>
+                    </div>
+                    
+                    <div>
+                        <div>
+                            <img src="./assets/img/blog_1.jpg" alt="Blog de Panamá">
+                        </div>
+
+                        <div>
+                            <h3>Descuble la belleza de Panamá ¡Casco Viejo y Bocas del Toro!</h3>
+                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
+                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
+                            <span>Seguir leyendo...</span>
+                        </div>
+
+                        <div>
+                            <img src="./assets/img/blog_2.jpg" alt="Blog de Panamá">
+                        </div>
+
+                        <div>
+                            <h3>Day Tour a San Blas ¡Emocionante Tour De Un Día!</h3>
+                            <p>Sumérgete en aguas cristalinas, explora islas exuberantes y disfruta de una experiencia inolvidable en este paraíso tropical.</p>
+                            <p>Desde la salida temprana de la Ciudad de Panamá hasta el regreso al atardecer, te guiaremos a través de un día lleno de actividades emocionantes y momentos relajantes. Con un itinerario cuidadosamente planificado que incluye tiempo para la exploración, el descanso y la deliciosa comida local, nuestro tour ofrece una escapada perfecta para aquellos que buscan desconectar de la rutina y sumergirse en la belleza natural de San Blas.</p>
+                            <span>Seguir leyendo...</span>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        </main>
+
+        <footer>
+            <div>
+                <div>
+                    <img src="./assets/img/logo.png" alt="logoEmpresa">
+                </div>
+                <div>
+                    &copy; 2024 Agencia de viajes - Conoce Panamá
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h3>Contáctanos</h3>
+                    <p>Escribenos a través de nuestro email</p>
+                    <form action="">
+                        <div>
+                            <label for="email">Email</label>
+                            <input type="email" name="email">
+                        </div>
+
+                        <div>
+                            <label for="mensaje">Mensaje</label>
+                            <textarea name="mensaje"></textarea>
+                        </div>
+                        
+                        <input type="submit" value="Enviar">
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
