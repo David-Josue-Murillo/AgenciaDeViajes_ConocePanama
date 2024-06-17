4125e3c070169708e87302bf22d43779a44c8385
David Murillo <david.josue.murillo23@gmail.com>
2024-06-16 22:05:57 -0500

    Dandole estilos al area de paquetes

--- index.php
+++ index.php
@@ -1,112 +1,156 @@
 <?php include_once 'includes/header.php'; ?>
-    <main>
-        <?php include_once 'includes/main_content.php'; ?>
+<main>
+    <?php include_once 'includes/main_content.php'; ?>
 
-        <section class="section__paquetes">
-            <div>
-                <h2>Conoce nuestros paquetes de viajes</h2>
-                <p>Descubre las mejores opciones de viajes que ofrecemos en Panamá</p>
-            </div>
-
-            <!-- Paquete de viajes  -->
-            <section class="paquetes__img">
-                <div>
-                    <div>
-                        <img src="./assets/img/paquete_1.webp" alt="Vuelo a Bocas del Toro">
-                    </div>
-                    <div>
-                        <img src="./assets/img/paquete_2.webp" alt="vuelo a isla Contadora">
-                    </div>
-                    <div>
-                        <img src="./assets/img/paquete_3.webp" alt="vuelo a isla Taboga">
-                    </div>
-                </div>
-            </section>
-        </section>
-
-        <!-- Cotización y reserva -->
-        <section class="section__cotizacion">
-            <div>
-                <div>
-                    <h3>Cotiza</h3>
-                </div>
-
-                <div>
-                    <h4>Buscar viaje</h4>
-                    <p>Escoge tu destino y el día de tu viaje</p>
-
-                    <form action="">
-                        <div>
-                            <label for="nombre">Nombre</label>
-                            <input type="text" name="nombre">
-                        </div>
-
-                        <div>
-                            <label for="email">Email</label>
-                            <input type="email" name="email">
-                        </div>
-
-                        <div>
-                            <label for="telefono">Telefono</label>
-                            <input type="tel" name="telefono">
-                        </div>
-
-                        <div>
-                            <label for="pasajeros">Número de pasajeros</label>
-                            <input type="number" name="pasajeros">
-                        </div>
-
-                        <div>
-                            <label for="destino">Destino</label>
-                            <select name="destino">
-                                <option value="Bocas del Toro">Bocas del Toro</option>
-                                <option value="Isla Contadora">Isla Contadora</option>
-                                <option value="Isla Taboga">Isla Taboga</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label for="fecha">Fecha</label>
-                            <input type="date" name="fecha">
-                        </div>
-
-                        <input type="submit" value="Solicitar">
-                    </form>
-                </div>
-            </div>
-        </section>
-    </main>
-
-    <footer>
-        <div>
-            <div>
-                <img src="./assets/img/logo.png" alt="logoEmpresa">
-            </div>
-            <div>
-                &copy; 2024 Agencia de viajes - Conoce Panamá
-            </div>
+    <section class="section__paquetes bg__general container-fluid d-flex justify-content-center flex-column">
+        <div class="w-100 text-center mt-5 ">
+            <h2 class="text-4xl text-prim">Conoce nuestros paquetes de viajes</h2>
+            <p class="text-2xl text-second">Descubre las mejores opciones de viajes que ofrecemos en Panamá</p>
         </div>
 
+        <!-- Paquete de viajes  -->
+        <section class="paquetes__img">
+            <div class="img__container">
+                <div class="card__paquetes">
+                    <div class="icon">
+                        
+                    </div>
+
+                    <strong> Bocas del Toro </strong>
+
+                    <div class="card__body">
+                    </div>
+
+                    <span>
+                        <div>
+                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
+                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
+                        </div>
+                    </span>
+                </div>
+
+                <div class="card__paquetes">
+                    <div class="icon">
+                        
+                    </div>
+
+                    <strong> Bocas del Toro </strong>
+
+                    <div class="card__body">
+                    </div>
+
+                    <span>
+                        <div>
+                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
+                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
+                        </div>
+                    </span>
+                </div>
+
+                <div class="card__paquetes">
+                    <div class="icon">
+                        
+                    </div>
+
+                    <strong> Bocas del Toro </strong>
+
+                    <div class="card__body">
+                    </div>
+
+                    <span>
+                        <div>
+                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
+                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <!-- Cotización y reserva -->
+    <section class="section__cotizacion">
         <div>
             <div>
-                <h3>Contáctanos</h3>
-                <p>Escribenos a través de nuestro email</p>
+                <h3>Cotiza</h3>
+            </div>
+
+            <div>
+                <h4>Buscar viaje</h4>
+                <p>Escoge tu destino y el día de tu viaje</p>
+
                 <form action="">
+                    <div>
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre">
+                    </div>
+
                     <div>
                         <label for="email">Email</label>
                         <input type="email" name="email">
                     </div>
 
                     <div>
-                        <label for="mensaje">Mensaje</label>
-                        <textarea name="mensaje"></textarea>
+                        <label for="telefono">Telefono</label>
+                        <input type="tel" name="telefono">
                     </div>
 
-                    <input type="submit" value="Enviar">
+                    <div>
+                        <label for="pasajeros">Número de pasajeros</label>
+                        <input type="number" name="pasajeros">
+                    </div>
+
+                    <div>
+                        <label for="destino">Destino</label>
+                        <select name="destino">
+                            <option value="Bocas del Toro">Bocas del Toro</option>
+                            <option value="Isla Contadora">Isla Contadora</option>
+                            <option value="Isla Taboga">Isla Taboga</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="fecha">Fecha</label>
+                        <input type="date" name="fecha">
+                    </div>
+
+                    <input type="submit" value="Solicitar">
                 </form>
             </div>
         </div>
-    </footer>
+    </section>
+</main>
+
+<footer>
+    <div>
+        <div>
+            <img src="./assets/img/logo.png" alt="logoEmpresa">
+        </div>
+        <div>
+            &copy; 2024 Agencia de viajes - Conoce Panamá
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <h3>Contáctanos</h3>
+            <p>Escribenos a través de nuestro email</p>
+            <form action="">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                </div>
+
+                <div>
+                    <label for="mensaje">Mensaje</label>
+                    <textarea name="mensaje"></textarea>
+                </div>
+
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+    </div>
+</footer>
 </body>
 
 </html>
