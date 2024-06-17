5a7ad7845de1d7e87ff67498417ef6ff6f07a208
David Murillo <david.josue.murillo23@gmail.com>
2024-06-16 22:12:26 -0500

    segmentado codigo html

--- index.php
+++ index.php
@@ -1,75 +1,10 @@
 <?php include_once 'includes/header.php'; ?>
 <main>
     <?php include_once 'includes/main_content.php'; ?>
-
-    <section class="section__paquetes bg__general container-fluid d-flex justify-content-center flex-column">
-        <div class="w-100 text-center mt-5 ">
-            <h2 class="text-4xl text-prim">Conoce nuestros paquetes de viajes</h2>
-            <p class="text-2xl text-second">Descubre las mejores opciones de viajes que ofrecemos en Panamá</p>
-        </div>
-
-        <!-- Paquete de viajes  -->
-        <section class="paquetes__img">
-            <div class="img__container">
-                <div class="card__paquetes">
-                    <div class="icon">
-                        
-                    </div>
-
-                    <strong> Bocas del Toro </strong>
-
-                    <div class="card__body">
-                    </div>
-
-                    <span>
-                        <div>
-                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
-                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
-                        </div>
-                    </span>
-                </div>
-
-                <div class="card__paquetes">
-                    <div class="icon">
-                        
-                    </div>
-
-                    <strong> Bocas del Toro </strong>
-
-                    <div class="card__body">
-                    </div>
-
-                    <span>
-                        <div>
-                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
-                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
-                        </div>
-                    </span>
-                </div>
-
-                <div class="card__paquetes">
-                    <div class="icon">
-                        
-                    </div>
-
-                    <strong> Bocas del Toro </strong>
-
-                    <div class="card__body">
-                    </div>
-
-                    <span>
-                        <div>
-                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
-                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
-                        </div>
-                    </span>
-                </div>
-            </div>
-        </section>
-    </section>
-
+    <?php include_once 'includes/paquetes_content.php'; ?>
+    
     <!-- Cotización y reserva -->
-    <section class="section__cotizacion">
+    <section class="section__cotizacion bg__general">
         <div>
             <div>
                 <h3>Cotiza</h3>
