fa2bd21abfb9a81321732dd60808f6410228bc3f
David Murillo <david.josue.murillo23@gmail.com>
2024-06-20 14:48:00 -0500

    Pagina de servicios: Rediseñando area donde estan los paquetes

--- destinos.php
+++ destinos.php
@@ -1,96 +1,158 @@
 <?php
-    include_once 'includes/header.php';
+include_once 'includes/header.php';
 ?>
 
-        <main>
-            <section class="section__content__destinos section__main">
-                <div class="section__main__info section__destinos__info">
-                    <h1 class="info__textPrim info__textPrim__content">Explorando Panamá a través de nuestros atractivos destinos turisticos</h1>
-                    <p class="info__textTerc">Beneficiate de nuestras ofertas especiales para descubrir los mejores sitios atractivos de Panamá</p>
+<main>
+    <section class="section__content__destinos section__main">
+        <div class="section__main__info section__destinos__info">
+            <h1 class="info__textPrim info__textPrim__content">Explorando Panamá a través de nuestros atractivos destinos turisticos</h1>
+            <p class="info__textTerc">Beneficiate de nuestras ofertas especiales para descubrir los mejores sitios atractivos de Panamá</p>
+        </div>
+
+        <div class="section__destinos__btn">
+            <button class="info__btn">
+                <a href="#">Lugares turísticos</a>
+            </button>
+
+            <button class="info__btn">
+                <a href="#">Acerca de Panamá</a>
+            </button>
+        </div>
+    </section>
+
+    <!-- Packages Start -->
+    <div class="container-fluid py-5">
+        <div class="container pt-5 pb-3">
+            <div class="text-center mb-3 pb-3">
+                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Destinos</h6>
+                <h1>Paquetes turísticos perfectos</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="assets/img/paquete_4.webp " alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Bocas del Toro</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>3 dias</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>2 Personas</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Descubre los lugares más increíbles de Panamá con nosotros.</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="section__destinos__btn">
-                    <button class="info__btn">
-                        <a href="#">Lugares turísticos</a>
-                    </button>
-
-                    <button class="info__btn">
-                        <a href="#">Acerca de Panamá</a>
-                    </button>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="assets/img/paquete_2.webp" alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Isla Contadora</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>6 dias</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>3 Personas</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Descubre las maravillas de la isla de Isla Contadora.</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </section>
-
-            <section class="section__paquetes">
-                <!-- Todos los paquetes de viajes  -->
-                <section class="paquetes__img">
-                    <div>
-                        <div>
-                            <img src="./assets/img/paquete_1.webp" alt="Bocas del Toro">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_2.webp" alt="Isla Contadora">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_3.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_4.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_5.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_6.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_7.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_8.webp" alt="Isla Taboga">
-                        </div>
-                        <div>
-                            <img src="./assets/img/paquete_9.webp" alt="Isla Taboga">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="assets/img/paquete_3.webp" alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Isla Taboga</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>4 dias</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>2 Personas</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Conoce los bellas aguas de las isla Taboga</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </section>
-
-                <section>
-                    <div>
-                        <h2>Blog de Panamá</h2>
-                    </div>
-                    
-                    <div>
-                        <div>
-                            <img src="./assets/img/blog_1.jpg" alt="Blog de Panamá">
-                        </div>
-
-                        <div>
-                            <h3>Descuble la belleza de Panamá ¡Casco Viejo y Bocas del Toro!</h3>
-                            <p>Descubre la Belleza de Panamá ¡Casco Viejo y Bocas del Toro!</p>
-                            <p>Panamá, un país lleno de contrastes y maravillas naturales, ofrece dos destinos imperdibles: Casco Viejo y Bocas del Toro. Desde la histórica ciudad de Panamá hasta las playas paradisíacas, este viaje te llevará a lugares fascinantes y emocionantes.</p>
-                            <span>Seguir leyendo...</span>
-                        </div>
-
-                        <div>
-                            <img src="./assets/img/blog_2.jpg" alt="Blog de Panamá">
-                        </div>
-
-                        <div>
-                            <h3>Day Tour a San Blas ¡Emocionante Tour De Un Día!</h3>
-                            <p>Sumérgete en aguas cristalinas, explora islas exuberantes y disfruta de una experiencia inolvidable en este paraíso tropical.</p>
-                            <p>Desde la salida temprana de la Ciudad de Panamá hasta el regreso al atardecer, te guiaremos a través de un día lleno de actividades emocionantes y momentos relajantes. Con un itinerario cuidadosamente planificado que incluye tiempo para la exploración, el descanso y la deliciosa comida local, nuestro tour ofrece una escapada perfecta para aquellos que buscan desconectar de la rutina y sumergirse en la belleza natural de San Blas.</p>
-                            <span>Seguir leyendo...</span>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="assets/img/paquete_4.webp" alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Hotel Candy Rose – Isla Grande</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>3 dias</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>6 Personas</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Discover amazing places of the world with us</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </section>
-            </section>
-        </main>
-
-        <!-- Footer Start -->
-        <?php include_once 'includes/footer.php'; ?>
-        <!-- Footer Start -->
-        
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="img/package-5.jpg" alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Thailand</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>2 Person</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Discover amazing places of the world with us</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="package-item bg-white mb-2">
+                        <img class="img-fluid" src="img/package-6.jpg" alt="">
+                        <div class="p-4">
+                            <div class="d-flex justify-content-between mb-3">
+                                <small class="m-0"><i class="fa fa-map-marker-alt text-primary mr-2"></i>Thailand</small>
+                                <small class="m-0"><i class="fa fa-calendar-alt text-primary mr-2"></i>3 days</small>
+                                <small class="m-0"><i class="fa fa-user text-primary mr-2"></i>2 Person</small>
+                            </div>
+                            <a class="h5 text-decoration-none" href="">Discover amazing places of the world with us</a>
+                            <div class="border-top mt-4 pt-4">
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>4.5 <small>(250)</small></h6>
+                                    <h5 class="m-0">$350</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <!-- Packages End -->
+</main>
+
+<!-- Footer Start -->
+<?php include_once 'includes/footer.php'; ?>
+<!-- Footer Start -->
+
+</div>
 </body>
 
 </html>
