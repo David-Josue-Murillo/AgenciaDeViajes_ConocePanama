06a410d89fc58b47c1af36a2c6bcb895f1402605
David Murillo <david.josue.murillo23@gmail.com>
2024-07-02 22:36:41 -0500

    Incluyendo nueva dependencia: Libreria fpdf, para la creación de pdf

--- contacto.php
+++ contacto.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'includes/header.php';
+include 'db/conexion.php';
 ?>
 
 <main class="section__contact">
@@ -16,56 +17,7 @@ include_once 'includes/header.php';
     <!-- Header End -->
 
     <!-- Reserva - Inicio -->
-    <div class="container-fluid booking mt-5">
-        <div class="container pb-5">
-            <div class="bg-light shadow" style="padding: 30px;">
-                <div class="row align-items-center" style="min-height: 60px;">
-                    <div class="col-md-10">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="mb-3 mb-md-0">
-                                    <select class="custom-select px-4" style="height: 47px;">
-                                        <option selected>Destino</option>
-                                        <option value="1">Destination 1</option>
-                                        <option value="2">Destination 1</option>
-                                        <option value="3">Destination 1</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mb-3 mb-md-0">
-                                    <div class="date py-1" id="date1" data-target-input="nearest">
-                                        <input type="date" class="form-control" placeholder="Depart Date" data-target="#date1" data-toggle="datetimepicker"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mb-3 mb-md-0">
-                                    <div class="date py-1" id="date2" data-target-input="nearest">
-                                        <input type="date" class="form-control" placeholder="Return Date" data-target="#date2" data-toggle="datetimepicker"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mb-3 mb-md-0">
-                                    <select class="custom-select px-4" style="height: 47px;">
-                                        <option selected>Duración</option>
-                                        <option value="1">Duration 1</option>
-                                        <option value="2">Duration 1</option>
-                                        <option value="3">Duration 1</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-2">
-                        <button class="info__btn" style="height: 47px; margin-top: -2px;"><a href="#area_cotizacion">Cotizar</a></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include 'includes/cotizacion.php'; ?>
     <!-- Reserva - Fin -->
 
     <!-- Contacto - Incio -->
--- php/area_cotizacion.php
+++ php/area_cotizacion.php
@@ -1,8 +1,43 @@
 <?php
 
-echo $_POST['destino']. "<br>";
-echo $_POST['fecha_inicio']. "<br>";
-echo $_POST['fecha_fin']. "<br>";
-echo $_POST['cantidad_personas']. "<br>";
+define('FPDF_FONTPATH', '../lib/fpdf/font/');
+require('../lib/fpdf/fpdf.php');
 
-echo "Funciona";
+if (isset($_POST['submit_cotizacion'])) {
+    $destino = $_POST['destino'] . "<br>";
+    $fecha_inicio = $_POST['fecha_inicio'] . "<br>";
+    $fecha_fin = $_POST['fecha_fin'] . "<br>";
+    $caantidad_personas = $_POST['cantidad_personas'] . "<br>";
+    echo "Funciona";
+
+    $pdf = new FPDF('P', 'mm', 'A4');
+
+    $pdf->AddPage(); // Agregar una página
+
+    // Agregar una imagen en el centro de la página
+    $pdf->Cell(0, 10, $pdf->Image('../assets/img/logo.png', 93, 10, 22, 25), 0, 0, 'C');
+    $pdf->Ln(25);
+    $pdf->SetFont('Courier', 'BIU', 14);
+
+    // Crear una celda, se especifica el ancho, alto, texto, borde, salto de línea y alineación del texto
+    $pdf->Cell(190, 20, 'LISTADO DE CEDULAS', 0, 1, 'C');
+
+    // Salto de linea
+    //$pdf->Ln(1);
+    $pdf->SetFont('Times', 'B', 12);
+    $pdf->Cell(62, 20, 'No', 0, 0, 'C');
+    $pdf->Cell(62, 20, 'Cedula', 0, 0, 'C');
+    $pdf->Cell(62, 20, 'Nombre', 0, 0, 'C');
+
+
+    // Establecer el tamaño de la fuente
+    $pdf->SetFont('Times', '', 12);
+
+
+    // Un pie de página con el número de página y la fecha en que se genera
+    $pdf->SetFont('Times', '', 8);
+    $pdf->Cell(0, 10, 'Pagina ' .   $pdf->PageNo() . '   ' . date('d-m-Y H:i:s'), 0, 0, 'C');
+
+    // Descargar el documento  
+    $pdf->Output('cedulas.pdf', 'D');
+}
