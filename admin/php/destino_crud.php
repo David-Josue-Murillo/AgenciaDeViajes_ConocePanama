54b114126005c14ccc6dbe8a05ed83ad65e9f57b
David Murillo <david.josue.murillo23@gmail.com>
2024-07-09 22:45:37 -0500

    Panel de administrador - Sección de detinos: Validando campos y estableciendo metodos de seuridad para evitar injeccion SLQ

--- admin/php/destino_crud.php
+++ admin/php/destino_crud.php
@@ -1,7 +1,28 @@
 <?php
+// Estableciendo la conexión a la base de datos
+include_once '../../db/conexion.php';
+
+// Iniciando una sesión
+if(!$_SESSION) {
+    session_start();
+}
+
 
 if(isset($_POST['submit_nuevo_destino'])){
-    echo 'Creando destino';
+    
+    // Obteniendo los datos del formulario
+    $nombre_destino = $_POST['destino'] ? mysqli_real_escape_string($conexion, $_POST['$destino']) : false;
+    $direccion = $_POST['direccion'] ? mysqli_real_escape_string($conexion, $_POST['$direccion']) : false; 
+    $descripcion = $_POST['descripcion'] ? mysqli_real_escape_string($conexion, $_POST['descripcion']) : false;
+    $url_imagen = $_POST['img-url'] ;
+
+    // Creando la tabla destinos
+    $sql = "INSERT INTO destinos (nombre_destino, direccion, descripcion, url_imagen) VALUES ('$nombre_destino', '$direccion', '$descripcion', '$url_imagen')";
+    $resultado = $conexion->query($sql);
+
+    if ($resultado->num_rows > 0) {
+        echo 'Destino creado correctamente';
+    }
 }
 
 if(isset($_POST['submit_modificar_destino'])){
