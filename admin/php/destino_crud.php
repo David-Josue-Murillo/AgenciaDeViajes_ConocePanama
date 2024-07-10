e570d052a7faff769247e27ca273857a7ddc7739
David Murillo <david.josue.murillo23@gmail.com>
2024-07-09 22:20:59 -0500

    Panel de administrador - Sección de detinos: Reecibiendo datos por post al crear y modificar los detinos

--- admin/php/destino_crud.php
+++ admin/php/destino_crud.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Pruebaaaa';
+if(isset($_POST['submit_nuevo_destino'])){
+    echo 'Creando destino';
+}
+
+if(isset($_POST['submit_modificar_destino'])){
+    echo 'Modificando destino';
+}
