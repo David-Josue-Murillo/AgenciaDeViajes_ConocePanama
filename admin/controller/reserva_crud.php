57c426e1f255318256276f408ab924c4fe27d894
David Murillo <david.josue.murillo23@gmail.com>
2024-07-13 16:38:56 -0500

    Consulta para modificar las reservas funcional

--- admin/controller/reserva_crud.php
+++ admin/controller/reserva_crud.php
@@ -9,53 +9,47 @@ if(!$_SESSION) {
     session_start();
 }
 
-// Recibiendo  datos para crear una nueva reserva
-if (isset($_POST['submit_nueva_reserva'])) {
-    // Recibiendo los datos del formulario para crear la reserva
-    $id_usuario = isset($_POST['id_usuario']) ? intval($_POST['id_usuario']) : false;
-    $id_destino = isset($_POST['id_destino']) ? intval($_POST['id_destino']) : false;
-    $id_paquete = isset($_POST['id_paquete']) ? intval($_POST['id_paquete']) : false;
-    $fecha_reserva = isset($_POST['fecha_reserva']) ? mysqli_real_escape_string($conexion, $_POST['fecha_reserva']) : false;
-    $estado = isset($_POST['estado']) ? $_POST['estado'] : false;
-
-    // Validando que los datos del formulario esten completos y sean correctos
-    if(empty($id_usuario) || empty($id_destino) || empty($id_paquete) || empty($fecha_reserva) || empty($estado)){
-        header('Location: ../admin.php');
-        exit();
-    }
-
-    // Crear la reserva en la base de datos
-    $sql = "INSERT INTO reservas (id_usuario, id_destino, id_paquete, fecha_reserva, estado) VALUES ('$id_usuario', '$id_destino', '$id_paquete', '$fecha_reserva', '$estado')";
-    $resultado = $conexion->query($sql);
-
-    if ($conexion->affected_rows > 0) {
-        $_SESSION['completado'] = 'Reserva creada correctamente';
-    } else {
-        $_SESSION['error'] = 'Error al crear la reserva';   
-    }
-
-    // Redireccionando al panel de administración
-    header('Location: ../admin.php');
-    exit();
-}
-
 // Recibiendo  datos para modificar una reserva existente
 if (isset($_POST['submit_modificar_reserva'])) {
     // Recibiendo los datos del formulario para modificar la reserva
     $id_reserva = isset($_POST['id_reserva']) ? intval($_POST['id_reserva']) : false;
-    $id_usuario = isset($_POST['id_usuario']) ? intval($_POST['id_usuario']) : false;
-    $id_destino = isset($_POST['id_destino']) ? intval($_POST['id_destino']) : false;
-    $id_paquete = isset($_POST['id_paquete']) ? intval($_POST['id_paquete']) : false;
-    $fecha_reserva = isset($_POST['fecha_reserva']) ? mysqli_real_escape_string($conexion, $_POST['fecha_reserva']) : false;
-    $estado = isset($_POST['estado']) ? $_POST['estado'] : false;
+    $nombre_usuario = isset($_POST['nombre_usuario']) ? mysqli_real_escape_string($conexion ,$_POST['nombre_usuario']) : false;
+    $nombre_paquete = isset($_POST['nombre_paquete']) ? mysqli_real_escape_string($conexion ,$_POST['nombre_paquete']) : false;
+    $descripcion = isset($_POST['descripcion_reserva']) ? mysqli_real_escape_string($conexion, $_POST['descripcion_reserva']) : false;
+    $precio_reserva = isset($_POST['precio_reserva']) ? intval($_POST['precio_reserva']) : false;
+    $metodo_pago = isset($_POST['metodo_pago']) ? mysqli_real_escape_string($conexion, $_POST['metodo_pago']) : false;
 
     // Validando que los datos del formulario esten completos y sean correctos
-    if(empty($id_usuario) || empty($id_destino) || empty($id_paquete) || empty($fecha_reserva) || empty($estado)){
+    if(empty($id_reserva) || empty($nombre_usuario) || empty($nombre_paquete) || empty($descripcion) || empty($precio_reserva) || empty($metodo_pago)){
         header('Location: ../admin.php');
         exit();
     }
 
-    $sql = "UPDATE reservas SET id_usuario = '$id_usuario', id_destino = '$id_destino', id_paquete = '$id_paquete', fecha_reserva = '$fecha_reserva', estado = '$estado' WHERE id_reserva = '$id_reserva'";
+    // Obtener solo el nombre del usuario
+    $nombre = explode(' ', $nombre_usuario);
+    $nombre_usuario = $nombre[0];
+
+    //Obtener el id del usuario
+    $sql = "SELECT id_usuario FROM usuarios WHERE nombre = '$nombre_usuario'";
+    $resultado = $conexion->query($sql);
+
+    if ($resultado->num_rows > 0) {
+        $id_usuario = $resultado->fetch_assoc();
+    }
+
+    // Obtener el id del paquete
+    $sql = "SELECT id_paquete FROM paquetes WHERE nombre_paquete = '$nombre_paquete'";
+    $resultado = $conexion->query($sql);
+
+    if ($resultado->num_rows > 0) {
+        $id_paquete = $resultado->fetch_assoc();
+    }
+
+    // Variable en donde se almacena el id del usuario y el id del paquete
+    $id_usuario = $id_usuario['id_usuario'];
+    $id_paquete = $id_paquete['id_paquete'];
+
+    $sql = "UPDATE reservas SET id_usuario = '$id_usuario', id_paquete = '$id_paquete', descripcion = '$descripcion', precio_venta = '$precio_reserva', metodo_pago = '$metodo_pago' WHERE id_reserva = '$id_reserva'";
     $resultado = $conexion->query($sql);
 
     if ($conexion->query($sql) === TRUE) {
