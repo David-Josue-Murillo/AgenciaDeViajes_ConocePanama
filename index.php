8d2e63a5c37d4d8cd2c5fe714ec7847871d4eaa3
David Murillo <david.josue.murillo23@gmail.com>
2024-07-14 22:53:00 -0500

    Cambiando información en el miniblog

--- index.php
+++ index.php
@@ -152,19 +152,19 @@ include 'admin/db/conexion.php';
                 <div class="col-lg-4 col-md-6 mb-4 pb-2">
                     <div class="blog-item">
                         <div class="position-relative">
-                            <img class="img-fluid w-100" src="assets/img/blog-3.jpg" alt="">
+                            <img class="img-fluid w-100" src="assets/img/paquete_8.webp" alt="">
                             <div class="blog-date">
-                                <h6 class="font-weight-bold mb-n1">01</h6>
-                                <small class="text-white text-uppercase">Jan</small>
+                                <h6 class="font-weight-bold mb-n1">06</h6>
+                                <small class="text-white text-uppercase">Agosto</small>
                             </div>
                         </div>
                         <div class="bg-white p-4">
                             <div class="d-flex mb-2">
-                                <a class="text-primary text-uppercase text-decoration-none" href="">Admin</a>
+                                <a class="text-primary text-uppercase text-decoration-none" href="">Playa</a>
                                 <span class="text-primary px-2">|</span>
                                 <a class="text-primary text-uppercase text-decoration-none" href="">Tours & Travel</a>
                             </div>
-                            <a class="h5 m-0 text-decoration-none" href="">Top Actividades Para Conocer la Cultura Congos</a>
+                            <a class="h5 m-0 text-decoration-none" href="">Paseo por playa blanca, disfruta de la playa y más</a>
                         </div>
                     </div>
                 </div>
